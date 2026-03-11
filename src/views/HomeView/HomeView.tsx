ba5e9f13be8d5a7168ddfbe3792c0404c37f9597
Raph <rafaelsprata@outlook.com>
2026-03-11 00:09:44 -0300

    first commit

---
+++ src/views/HomeView/HomeView.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "./HomeView.module.scss";
+
+export default function HomeView() {
+  return (
+    <div  className={styles.homeview}>
+      <Image
+        src="/logo.png"
+        alt="Hogwarts Library Logo"
+        width={800} 
+        height={400} 
+        className={styles.logo}
+      />
+    </div>
+  );
+}
