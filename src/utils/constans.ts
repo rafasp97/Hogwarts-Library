bad6b0c8acd87f607084306a0f42c6057dcee037
Raph <rafaelsprata@outlook.com>
2026-03-13 00:33:02 -0300

    style: Changes to the layout of the Hogwarts houses section.

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,16 +1,16 @@
 export const months: string[] = [
-  "January",
-  "February",
+  "Jan",
+  "Feb",
   "March",
   "April",
   "May",
   "June",
   "July",
   "August",
-  "September",
+  "Sept",
   "October",
-  "November",
-  "December"
+  "Nov",
+  "Dec"
 ];
 
 export const paramsDetails = ["dateOfBirth", "house", "patronus", "actor", "alive"];
@@ -22,3 +22,31 @@ export const bgsHome: string[] = [
 ];
 
 export const ballColors = ["#6D1A12", "#daac63", "#252e6d", "#1C4147"]; 
+
+
+export const houses = [
+  { 
+    name: "Gryffindor",
+    title: "Brave at Heart", 
+    src: "/images/houses/gryffindor.jpg",
+    description: "Courage and boldness define its members. They face challenges head-on even when everything seems impossible. Members of this house value bravery, honor, and determination. They stand up for what is right, protect others, and are never afraid to face danger when justice is at stake."
+  },
+  { 
+    name: "Hufflepuff",
+    title: "Loyal & Kind", 
+    src: "/images/houses/hufflepuff.jpg",
+    description: "Loyalty, patience, and dedication are their trademarks. Always ready to help and guided by a strong sense of fairness. Hufflepuff values hard work, loyalty, and kindness. Its members believe in fairness, helping others, and showing patience and dedication in everything they do."
+  },
+  { 
+    name: "Ravenclaw",
+    title: "Wit & Wisdom", 
+    src: "/images/houses/ravenclaw.jpg",
+    description: "Intelligence and creativity shape their spirit. They seek knowledge and love learning in original ways. Ravenclaw gathers those with curious minds and creative thinking. Its members value knowledge, insight, and the constant pursuit of learning and new ideas."
+  },
+  { 
+    name: "Slytherin",
+    title: "Cunning & Pride", 
+    src: "/images/houses/slytherin.jpg",
+    description: "Ambition, cunning, and determination mark its members, who pursue their goals with strategy. Slytherin values ambition, leadership, and resourcefulness. Its members are strategic thinkers who pursue greatness and are willing to take bold paths to achieve success."
+  }
+];
--- src/views/HouseView/HouseView.tsx
+++ src/views/HouseView/HouseView.tsx
@@ -1,89 +1,45 @@
 import Image from "next/image";
 import styles from "./HouseView.module.scss";
-import { flags } from "@/utils/flags";
-import { useState, useEffect, useRef } from "react";
+import { houses } from "@/utils/constans";
 import { useWindowSize } from "@/hooks/useWindowsSize";
+import { useState } from "react";
 
 export default function HouseView() {
-  const [index, setIndex] = useState(0);
   const { width } = useWindowSize();
-  const sliderRef = useRef<HTMLDivElement | null>(null);
+  const isDesktop = width > 640;
 
-  const next = () => {
-    setIndex((prev) => (prev + 1) % flags.length);
-  };
+  const [current, setCurrent] = useState(0);
 
-  const prev = () => {
-    setIndex((prev) => (prev - 1 + flags.length) % flags.length);
-  };
-
-  useEffect(() => {
-    if (!sliderRef.current || width > 1024) return;
-
-    const slider = sliderRef.current;
-
-    const handleScroll = () => {
-      const scrollLeft = slider.scrollLeft;
-      const containerWidth = slider.offsetWidth;
-      let newIndex = Math.round(scrollLeft / containerWidth);
-      newIndex = Math.max(0, Math.min(newIndex, flags.length - 1));
-
-      setIndex((prevIndex) => {
-        if (prevIndex !== newIndex) return newIndex;
-        return prevIndex;
-      });
-    };
-
-    slider.addEventListener("scroll", handleScroll);
-
-    return () => slider.removeEventListener("scroll", handleScroll);
-  }, [width]);
+  const house = houses[current];
+  const reverse = isDesktop && current % 2 !== 0;
 
   return (
     <main className={styles.houseview} id="houses">
-      <header className={styles.logoContainer}>
-        <Image
-          src="/images/houses1.png"
-          alt="Hogwarts Library Logo"
-          width={300}
-          height={300}
-          className={styles.logo}
-        />
-      </header>
-      <section>
-        <div className={styles.flagsSlider} ref={sliderRef}>
-          <div
-            className={styles.flagsWrapper}
-            style={{ 
-              transform: width > 640 ? `translateX(-${index * 100}%)` : undefined 
-            }}
-          >
-            {flags.map((flag) => (
-              <div key={flag.name} className={styles.flagContainer}>
-                <Image
-                  src={flag.src}
-                  alt={`Flag of ${flag.name}`}
-                  width={300}
-                  height={300}
-                  className={styles.flag}
-                />
-              </div>
-            ))}
+      <section className={styles.section}>
+        <div className={`${styles.container} ${reverse ? styles.reverse : ""}`}>
+          <Image
+            src={house.src}
+            alt={`Flag of ${house.name}`}
+            width={300}
+            height={300}
+            className={styles.house}
+          />
+          <div className={styles.textContainer}>
+            <h2>{house.name}</h2>
+            <h3>{house.title}</h3>
+            <p>{house.description}</p>
           </div>
         </div>
-        <div className={styles.textContainer}>
-          <h2>{flags[index].name}</h2>
-          <p>
-            {flags[index].description} 
-            {width > 864 ? flags[index].extension : ""}
-          </p>
-          {width > 640 &&
-            <div className={styles.controls}>
-              <button onClick={prev}>◀</button>
-              <button onClick={next}>▶</button>
-            </div>
-          }
+        <div className={styles.indicators}>
+          {houses.map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.dot} ${current === index ? styles.active : ""}`}
+              onClick={() => setCurrent(index)}
+            />
+          ))}
         </div>
+
       </section>
     </main>
   );
